a37173fcf5b057a6caa51c0c54be286b7295e753
caducoder <cadudrop@hotmail.com>
2023-08-03 10:26:50 -0300

    feat: table to display available courses

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: 1, name: 'Angular', category: 'Front-End', price: 27.90 }
+  ];
+  displayedColumns: string[] = ['name', 'category', 'price'];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
