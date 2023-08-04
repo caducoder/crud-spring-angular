26f78c81902d8d6c2a132fd91322117cad5b7c7f
caducoder <cadudrop@hotmail.com>
2023-08-04 10:52:28 -0300

    feat: fetch courses from json file

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,11 +10,12 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns: string[] = ['name', 'category', 'price'];
 
   //coursesService: CoursesService;
 
+  // injeta o serviço
   constructor(private coursesService: CoursesService) {
     //this.coursesService = new CoursesService();
     this.courses = this.coursesService.listAll();
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Course } from '../model/course';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly BASE_API = '/assets/courses.json'
+
+  // injeto o módulo HttpClient
   constructor(private httpClient: HttpClient) { }
 
   listAll() {
-    return [
-      { _id: 1, name: 'Angular', category: 'Front-End', price: 27.90 },
-      { _id: 2, name: 'React', category: 'Front-End', price: 27.90 },
-      { _id: 3, name: 'Vue', category: 'Front-End', price: 27.90 }
-    ]
+    return this.httpClient.get<Course[]>(this.BASE_API)
+      .pipe(
+        first(), // informa que quer apenas a primeira resposta, finalizando a inscrição
+        tap(courses => console.log(courses))
+      )
   }
 }
